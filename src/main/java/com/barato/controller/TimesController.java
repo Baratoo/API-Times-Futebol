8f1411d5945072dbdd90a086e778fdad25218f20
Baratoo <kaique_luiz1@hotmail.com>
2024-10-01 10:02:09 -0300

    update and delete

--- src/main/java/com/barato/controller/TimesController.java
+++ src/main/java/com/barato/controller/TimesController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,7 +71,11 @@ public class TimesController {
 		
 		timesRepository.save(timeExistente);
 		return timeExistente;
-		
+	}
+	
+	@DeleteMapping("{id}")
+	public void deleteTeam (@PathVariable Long id) {
+		timesRepository.deleteById(id);
 	}
 	
 	
